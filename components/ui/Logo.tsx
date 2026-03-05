4900dd944bef51a727e71e1db0b0c9f4373c8b5c
MrJavokhir <mr.javokhir@gmail.com>
2026-03-05 16:10:30 +0500

    fix: update Logo SVG to correct graduation cap with tassel, match reference design

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -24,17 +24,16 @@ export default function Logo({
     <div className={`flex items-center gap-2.5 ${className}`}>
       {/* Blue rounded icon box */}
       <div
-        className={`flex items-center justify-center bg-indigo-600 rounded-xl text-white flex-shrink-0 ${iconSize}`}
+        className={`flex items-center justify-center bg-[#3259E8] rounded-2xl text-white flex-shrink-0 ${iconSize}`}
       >
-        {/* Academic Cap SVG (Heroicons v2 solid) */}
+        {/* Graduation cap SVG with tassel */}
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          className="w-[60%] h-[60%]"
+          className="w-[58%] h-[58%]"
         >
-          <path d="M11.7 2.805a.75.75 0 01.6 0A60.65 60.65 0 0122.83 8.72a.75.75 0 01-.231 1.337 49.949 49.949 0 00-9.902 3.912l-.003.002c-.114.06-.227.119-.342.18a.75.75 0 01-.707 0A50.88 50.88 0 007.5 12.173v-.224c0-.131.067-.248.172-.311a54.615 54.615 0 014.653-2.52.75.75 0 00-.65-1.352 56.123 56.123 0 00-4.78 2.589 1.858 1.858 0 00-.859 1.228 49.803 49.803 0 00-4.634-1.527.75.75 0 01-.231-1.337A60.653 60.653 0 0111.7 2.805z" />
-          <path d="M13.06 15.473a48.45 48.45 0 017.666-3.282c.134 1.414.22 2.843.255 4.284a.75.75 0 01-.46.711 47.87 47.87 0 00-8.105 4.342.75.75 0 01-.832 0 47.87 47.87 0 00-8.104-4.342.75.75 0 01-.461-.71c.035-1.442.121-2.87.255-4.286.921.304 1.83.634 2.726.99v1.27a1.5 1.5 0 001.444 1.5c.522 0 1.023-.215 1.372-.596l1.22-1.332c.123-.133.268-.242.428-.322.695-.347 1.408-.667 2.133-.961zM11.25 18v2.25a.75.75 0 001.5 0V18a.75.75 0 00-1.5 0z" />
+          <path d="M5 13.18v4L12 21l7-3.82v-4L12 17l-7-3.82zM12 3L1 9l11 6 9-4.91V17h2V9L12 3z" />
         </svg>
       </div>
       {/* Text */}
